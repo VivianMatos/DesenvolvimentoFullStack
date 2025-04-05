2d1a0c2ac7039379dda829d67d56bdd68f585cbc
VivianMatos <99364423+VivianMatos@users.noreply.github.com>
2025-04-05 17:28:07 -0300

    Create 02-inserir-e-consultar.sql

---
+++ SQL/02-inserir-e-consultar.sql
@@ -0,0 +1,8 @@
+-- Inserir dados
+INSERT INTO usuarios (id, nome, email)
+VALUES 
+(1, 'Vivian Matos', 'vivianmatos.eng@gmail.com'),
+(2, 'José Roberto Leite', 'joserobertoffc@gmail.com');
+
+-- Consultar todos os usuários
+SELECT * FROM usuarios;
